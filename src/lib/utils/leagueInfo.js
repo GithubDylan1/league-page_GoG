717121e14804fd26343883ad2cdb802e5fa8be01
GithubDylan1 <112986422+GithubDylan1@users.noreply.github.com>
2023-08-07 22:20:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -6,8 +6,8 @@ export const dynasty = false; // true for dynasty leagues, false for redraft and
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
-export const homepageText = 
-  <p>Ace, please we need a fACE.</p>
+// export const homepageText = 
+//  <p>Ace, please we need a fACE.</p>
 
 /*   STEP 3   */
 /*
